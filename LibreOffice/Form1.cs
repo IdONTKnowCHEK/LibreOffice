a257a7356f3f2f82edb083ebb1b1216a4824bc96
“chak12346789@gmail.com” <chak12346789@gmail.com>
2023-10-20 15:46:19 +0800

    save directly without press btn

--- LibreOffice/Form1.cs
+++ LibreOffice/Form1.cs
@@ -12,9 +12,12 @@ namespace LibreOffice
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        private string _param;
+
+        public Form1(string param)
         {
             InitializeComponent();
+            _param = param; 
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -29,12 +32,21 @@ namespace LibreOffice
                 return;
             loCalc.bStartOpenOfficeLoader();
             loCalc.bCreateWorkbook();
-            loCalc.bSetValue("A1", 12345555);
-            loCalc.bSetValue("B1", 2468);
-            loCalc.bSetFormula("C1", "=A1+B1");
-            loCalc.bSetText("A2", "Text entered here.");
-            loCalc.bSetDate("A3", 2023, 12, 25);
-            loCalc.bSetBackgroundColor("A3", Color.Red);
+
+            if (_param != string.Empty)
+            {
+                loCalc.bSetText("A1", _param);
+            }
+            else
+            {
+                loCalc.bSetValue("A1", 12345555);
+                loCalc.bSetValue("B1", 2468);
+                loCalc.bSetFormula("C1", "=A1+B1");
+                loCalc.bSetText("A2", "Text entered here.");
+                loCalc.bSetDate("A3", 2023, 12, 25);
+                loCalc.bSetBackgroundColor("A3", Color.Red);
+            }
+            
             string filePath = "C:\\file\\file.ods";
             string strFilePath = loCalc.strSaveWorkbook(filePath);
             Console.WriteLine("Saved as: " + strFilePath);
--- LibreOffice/Program.cs
+++ LibreOffice/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,11 +13,38 @@ namespace LibreOffice
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            string param = args.Length > 0 ? args[0] : string.Empty;
+            // Application.Run(new Form1(param));
+            LOCalc.Class1 loCalc = new LOCalc.Class1();
+            if (loCalc.bOpenOfficeInstalled == false)
+                return;
+            loCalc.bStartOpenOfficeLoader();
+            loCalc.bCreateWorkbook();
+
+            if (param != string.Empty)
+            {
+                loCalc.bSetText("A1", param);
+            }
+            else
+            {
+                loCalc.bSetValue("A1", 12345555);
+                loCalc.bSetValue("B1", 2468);
+                loCalc.bSetFormula("C1", "=A1+B1");
+                loCalc.bSetText("A2", "Text entered here.");
+                loCalc.bSetDate("A3", 2023, 12, 25);
+                loCalc.bSetBackgroundColor("A3", Color.Red);
+            }
+
+            string filePath = "C:\\file\\file.ods";
+            string strFilePath = loCalc.strSaveWorkbook(filePath);
+            Console.WriteLine("Saved as: " + strFilePath);
+            loCalc.bCloseWorkbook();
+
+            Application.Exit();
         }
     }
 }
